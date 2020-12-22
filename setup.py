136bc669a47909c6e7d6d7ffa432eaab94f21c5b
Ahmed Abdulrahman <63383794+Ahmed-0357@users.noreply.github.com>
2020-12-22 13:35:08 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ classifiers = [
 
 setup(
     name='aisaratuners',
-    version='1.4.7',
+    version='1.4.8',
     description='leveraging aisara algorithm for effective hyperparameter tuning',
     license = 'LICENSE.txt',
     long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
